4a8f92084be6219bb1772ef031209e8ef6915919
Gabriel Lumbi <gabriel.lumbi@gmail.com>
2015-02-09 15:21:12 -0500

    Added Button subclass PeachButton.

---
+++ src/main/java/com/lumbi/peach/app/PeachButton.java
@@ -0,0 +1,23 @@
+package com.lumbi.peach.app;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+/**
+ * Created by gabriellumbi on 15-02-09.
+ */
+public class PeachButton extends Button{
+
+    public PeachButton(Context context) {
+        super(context);
+    }
+
+    public PeachButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PeachButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
